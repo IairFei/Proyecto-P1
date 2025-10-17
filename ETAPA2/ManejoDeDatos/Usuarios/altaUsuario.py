7e87d861613fd31c7c8b40b45176396b5acf2a14
Iair Feigelman <feigelmaniair@gmail.com>
2025-10-17 12:48:07 -0300

    Implementando el uso de Faker y la vaidacion de que no existan 2 alumnos con el mismo nombre de usuario

--- ETAPA2/ManejoDeDatos/Usuarios/altaUsuario.py
+++ ETAPA2/ManejoDeDatos/Usuarios/altaUsuario.py
@@ -1,6 +1,23 @@
 import json
+from ManejoDeDatos.Usuarios.usuarios import validarNombreUsuarioEnSistema
+from faker import Faker
+fake = Faker()
+#fake = Faker('es_AR')
 
 #Alta en archivo JSON
+def nombreUsuarioRepetido(nombreAlumno, apellidoAlumno):
+        contador = 1
+        while existeUsuario is not None and len(nombreAlumno) > contador:
+            usuario = nombreAlumno[0].lower() + nombreAlumno[contador].lower() + apellidoAlumno.lower()
+            contador += 1
+            existeUsuario = validarNombreUsuarioEnSistema(usuario)
+        if len(nombreAlumno) == contador and existeUsuario is not None:
+            sufijo = 1
+            while existeUsuario is not None:
+                usuario = nombreAlumno.lower() + apellidoAlumno.lower() + str(sufijo)
+                sufijo += 1
+                existeUsuario = validarNombreUsuarioEnSistema(usuario)
+
 def altaEnSistema(usuario, nombreAlumno, apellidoAlumno):
     try:
         with open('ETAPA2/Archivos/usuarios.json', 'r') as users:
@@ -27,7 +44,6 @@ def altaEnSistema(usuario, nombreAlumno, apellidoAlumno):
         with open('ETAPA2/Archivos/usuarios.json', 'w') as users:
             json.dump(datos_sistema, users, indent=4)
         return True
-        
     except Exception as e: 
         print(f"Error: {e}")
         return None
@@ -35,10 +51,13 @@ def altaEnSistema(usuario, nombreAlumno, apellidoAlumno):
 def altaUsuario():
     try:
         with open('ETAPA2/Archivos/usuarios.csv', 'a') as archivo:
-            nombreAlumno = input("Ingrese el nombre del alumno: ").strip()
+            nombreAlumno = input("Ingrese el primer nombre del alumno: ").strip()
             apellidoAlumno = input("Ingrese el apellido del alumno: ").strip()
             print("Ingrese los datos del nuevo usuario:")
             usuario = nombreAlumno[0].lower() + apellidoAlumno.lower()
+            existeUsuario = validarNombreUsuarioEnSistema(usuario)
+            if existeUsuario is not None:
+                nombreUsuarioRepetido(nombreAlumno, apellidoAlumno)
             contrasena = input("Ingrese la contraseña: ").strip()
             tipo_usuario = int(input("Ingrese el tipo de usuario (1- Administrator/2- Standard): "))
             if tipo_usuario == 1:
@@ -50,9 +69,30 @@ def altaUsuario():
             dadoDeAlta = altaEnSistema(usuario,nombreAlumno,apellidoAlumno)
             if dadoDeAlta == False or dadoDeAlta is None:
                 print("No se pudo dar de alta al usuario en el sistema.")
-                archivo.detach
                 usuario = None
             return usuario
     except Exception as e:
         print(f"Error: {e}")
         return None
+
+
+
+def inicializarUsuariosFake():
+    for _ in range(10):
+        nombreAlumno = fake.first_name()
+        apellidoAlumno = fake.last_name()
+        usuario = nombreAlumno[0].lower() + apellidoAlumno.lower()
+        existeUsuario = validarNombreUsuarioEnSistema(usuario)
+        if existeUsuario is not None:
+            nombreUsuarioRepetido(nombreAlumno, apellidoAlumno)
+        contrasena = fake.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
+        tipo_usuario = "User"
+        try:
+            with open('ETAPA2/Archivos/usuarios.csv', 'a') as archivo:
+                archivo.writelines(f"{usuario},{contrasena},{tipo_usuario}\n")
+            dadoDeAlta = altaEnSistema(usuario,nombreAlumno,apellidoAlumno)
+            if dadoDeAlta == False or dadoDeAlta is None:
+                print(f"No se pudo dar de alta al usuario {usuario} en el sistema.")
+        except Exception as e:
+            print(f"Error: {e}")
+    
